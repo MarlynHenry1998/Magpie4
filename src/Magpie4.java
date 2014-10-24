6aa82984ee6cf2b8207c7daf02598dbf081d435c
MarlynHenry1998 <mimehenry@yahoo.com>
2014-10-24 11:33:03 -0400

    idk
    idk

--- src/Magpie4.java
+++ src/Magpie4.java
@@ -59,6 +59,11 @@ public class Magpie4
             response = transformIWantStatement(statement);
         }
 
+        else if (findKeyword(statement, "I like you", 0) >= 0)
+        {
+            response = transformISomethingStatement(statement);
+        }
+
 		else
 		{
 			// Look for a two word (you <something> me)
@@ -107,9 +112,22 @@ public class Magpie4
                     .length() - 1);
         }
         int psn = findKeyword (statement, "I want", 0);
-        String restOfStatement = statement.substring(psn + 9).trim();
+        String restOfStatement = statement.substring(psn + 6).trim();
         return "Would you really be happy if you had " + restOfStatement + "?";
 	}
+
+    private String transformISomethingStatement(String statement) {
+            statement = statement.trim();
+            String lastChar = statement.substring(statement
+                    .length() - 1);
+            if (lastChar.equals(".")) {
+                statement = statement.substring(0, statement
+                    .length() - 1);
+            }
+        int psn = findKeyword (statement, "I like you", 0);
+        String restOfStatement = statement.substring(psn + 1).trim();
+        return "Why do you " + restOfStatement + "?";
+    }
 
 	
 	
